76bdaa68d8d5bfc50367e9654262f0c4aa7b8056
Dane Dukic <ddukic86+22@gmail.com>
2022-04-28 14:43:51 +0200

    Komentari

--- cypress/integration/checkbox.spec.js
+++ cypress/integration/checkbox.spec.js
@@ -1,8 +1,14 @@
+// cemu sluzi ova linija ispod?
 /// <reference types="cypress" />
 
 import { navigateTo } from "../support/page_objects/navigationPage"
 
+// Sve ukupno je pristojno odradjen posao
+// Postavicu samo par pitanja
 
+// svaki test mi pada zbog neke greske u aplikaciji 
+// browser konzola -- Uncaught TypeError: e(...).setup is not a function
+// vidi da li ovo moze nekako da se ignorise pomocu Cypress-a
 
 describe('checkbox cheking', () => {
 
@@ -13,9 +19,14 @@ describe('checkbox cheking', () => {
 
   it('cheking the checkbox is checked', () => {
 
+    // kako si pronalazio ove selektore, moze li krace da se napisu a da budu jedinstveni?
+    // poenta page object pattern-a je da se ovakve stvari pisu u page-ovima.
+    // tvoj dobar primer je form_Submission funkcija
     cy.get(':nth-child(1) > .element-list > .menu-list > #item-1').click()
+    
 
-    cy.get('#tree-node-home').check({ force: true }).should('be.checked')
+    cy.get('#tree-node-home').check({ force: true }).should('be.checked') // radi li i bez force: true?
+  
     
 
   })
--- cypress/integration/homePage.spec.js
+++ cypress/integration/homePage.spec.js
@@ -12,7 +12,7 @@ describe('Home page test', () => {
     cy.get('.card').each(($el, index, $list) => {
 
       cy.get('.card').eq(index).click()
-      cy.wait(1000)
+      cy.wait(1000)  // gledaj da nigde ne koristis fiksnu vrednost wait-a. Sigurno ima neki nacin da se ceka na neki dogadjaj da se desi ili zavrsi
       cy.get(':nth-child(1) > .group-header > .header-wrapper').should('exist')
       cy.visit('/')
       cy.wait(1000)
